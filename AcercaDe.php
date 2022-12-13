ac81ff1a7b310fbab70cd554b2c4c057d96a01cc
Erick Ortiz <al191362@edu.uaa.mx>
2022-12-12 18:26:39 -0600

    cambioAcercade

--- AcercaDe.php
+++ AcercaDe.php
@@ -147,7 +147,7 @@
   </div><!-- /capa -->
 
   
-  <section id="servicos">
+  <section id="servicos" style="background-color: rgb(255,255,255,0.5)">
     <div class="container">
 
       <div class="row">
@@ -155,7 +155,7 @@
         <div class="col-xs-12 col-sm-6 col-md-6">
           <div class="row albuns">
             <div class="col-md-6">
-              <img src="imagens/Ray.jpg" class="img-responsive img-rounded">
+              <img src="imagens/urry.jpg" class="img-responsive img-rounded">
             </div>
 
 
